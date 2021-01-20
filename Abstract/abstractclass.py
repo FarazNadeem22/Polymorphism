10219783cd07c43e326948c9fbb83d4ba9d8f4f9
Faraz Nadeem <66399838+FarazNadeem22@users.noreply.github.com>
2021-01-20 00:29:56 -0500

    Update abstractclass.py

--- Abstract/abstractclass.py
+++ Abstract/abstractclass.py
@@ -3,16 +3,19 @@ from abc import *
 class Vehicle(ABC):
     @abstractmethod
     def no_of_wheels(self):
+        """This is your abstract method that will be implemented in the child-class see readme.md for more information"""
         pass
     def name_of_Vihicle(self):
         print("The Faraz Car")
 
 class Bus(Vehicle):
     def no_of_wheels(self):
+        """Implementing the the abstracrt method from the parent class here"""
         return 8
 
 class Car(Vehicle):
     def no_of_wheels(self):
+        """Implementing the the abstracrt method from the parent class here"""
         return 4
 
 
