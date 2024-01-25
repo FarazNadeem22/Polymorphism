df65b5d067693c7e1aef0297dfb53badda0a7574
Faraz Nadeem <66399838+FarazNadeem22@users.noreply.github.com>
2024-01-25 11:04:00 -0500

    Update Operator Overloading.py
    comments and docstrings added

--- Overloading/Operator Overloading.py
+++ Overloading/Operator Overloading.py
@@ -1,20 +1,41 @@
 class Book:
     def __init__(self, pages):
+        """Constructor method to initialize the Book object with the given number of pages.
+
+        Args:
+            pages (int): Number of pages in the book.
+        """
         self.pages = pages
 
     def __add__(self, other):
-        object = Book(self.pages+other.pages)
-        return object
+        """Overloaded addition operator to add two Book objects.
+
+        Args:
+            other (Book): Another Book object to be added.
+
+        Returns:
+            Book: A new Book object with the sum of pages from the two input books.
+        """
+        new_book = Book(self.pages + other.pages)
+        return new_book
 
     def display(self):
+        """Displays the number of pages in the book."""
         print(self.pages)
 
+# Creating instances of the Book class
 b1 = Book(100)
 b2 = Book(100)
 b3 = Book(200)
 
-print(b1+b2)
-print("*"*50)
-pages = b1+b2+b3
-print(pages.pages)
-pages.display()
+# Using the overloaded addition operator to add b1 and b2
+result_book = b1 + b2
+print(result_book)
+print("*" * 50)
+
+# Using the overloaded addition operator to add b1, b2, and b3
+total_pages = b1 + b2 + b3
+print(total_pages.pages)
+
+# Displaying the total number of pages
+total_pages.display()
