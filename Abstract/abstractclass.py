c595f140abbb645dc22f361119bed44ee6d21ec4
Faraz Nadeem <66399838+FarazNadeem22@users.noreply.github.com>
2024-01-25 10:58:55 -0500

    Update abstractclass.py
    comments and docstrings added

--- Abstract/abstractclass.py
+++ Abstract/abstractclass.py
@@ -1,27 +1,48 @@
-from abc import *
+from abc import ABC, abstractmethod
 
 class Vehicle(ABC):
     @abstractmethod
     def no_of_wheels(self):
-        """This is your abstract method that will be implemented in the child-class see readme.md for more information"""
+        """Abstract method to be implemented by child classes.
+        
+        Returns:
+            int: Number of wheels for the specific vehicle.
+        """
         pass
-    def name_of_Vihicle(self):
+    
+    def name_of_Vehicle(self):
+        """Prints the default vehicle name."""
         print("The Faraz Car")
 
 class Bus(Vehicle):
     def no_of_wheels(self):
-        """Implementing the the abstracrt method from the parent class here"""
+        """Implements the abstract method from the parent class.
+        
+        Returns:
+            int: Number of wheels for a bus (in this case, 8).
+        """
         return 8
 
 class Car(Vehicle):
     def no_of_wheels(self):
-        """Implementing the the abstracrt method from the parent class here"""
+        """Implements the abstract method from the parent class.
+        
+        Returns:
+            int: Number of wheels for a car (in this case, 4).
+        """
         return 4
 
-
+# Creating an instance of the Car class
 car = Car()
-car.name_of_Vihicle()
+
+# Calling the name_of_Vehicle method to print the default vehicle name
+car.name_of_Vehicle()
+
+# Calling the no_of_wheels method to get the number of wheels for the car
 print(car.no_of_wheels())
 
+# Creating an instance of the Bus class
 bus = Bus()
+
+# Calling the no_of_wheels method to get the number of wheels for the bus
 print(bus.no_of_wheels())
